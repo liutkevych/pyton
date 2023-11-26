fcb5042adf0b50e2782b0ce193ca1294431043b7
liutkevych <oli@keepit.com>
2023-11-26 20:38:09 +0100

    Re.sub() for regexp

---
+++ format.py
@@ -0,0 +1,9 @@
+import re
+
+name = input("What's you name? ").strip()
+if matches := re.search(r"^(.+), *(.+)$", name):
+    last = matches.group(1)
+    first = matches.group(2)
+    name = f"{first} {last}"
+
+print(f"Hello, {name}")
