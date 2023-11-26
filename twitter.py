dc5c0f0d605361d9f773998d19a0e7eb2e4a4d05
liutkevych <oli@keepit.com>
2023-11-26 20:50:17 +0100

    End of regexp

--- twitter.py
+++ twitter.py
@@ -2,7 +2,6 @@ import re
 
 url = input("URL: ").strip()
 
-username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
-
-
-print(f"Username: {username}")
+matches = re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)$", url, re.IGNORECASE)
+if matches:
+    print(f"Username:", matches.group(1))
