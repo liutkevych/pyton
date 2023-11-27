f2a4156bbddeb0f34268118a6e956f5baab4621d
liutkevych <oli@keepit.com>
2023-11-27 13:28:04 +0100

    Add custom method to a class

--- student.py
+++ student.py
@@ -1,15 +1,45 @@
 class Student:
-    ...
+    def __init__(self, name, house, patronus):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Lviv", "New York", "London", "Krakow"]:
+            raise ValueError("House is unecceptable")
+        self.name = name
+        self.house = house
+        self.patronus = patronus
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+    
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "🐴"
+            case "Otter":
+                return "🤦‍♂️"
+            case "Jack Russell terrier":
+                return "🐶"
+            case _:
+                return "🧚‍♀️"
+
+
 
 def main():
     student = get_student()
-    print(f"{student.name} is from {student.house}")
+    print("Expecto Patronum!")
+    print(student.charm())
+
 
 def get_student():
-    student = Student()
-    student.name = input("What's name? ")
-    student.house = input("House: ")
-    return student
+    name = input("What's name? ")
+    house = input("House: ")
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
+    # try:
+    #     return Student(name, house)
+    # except ValueError:
+    #     print("You should put correct data")
+
 
 if __name__ == "__main__":
     main()
