61efc7d524ea03040755a2bf93a754ede59e6da2
liutkevych <oli@keepit.com>
2023-11-28 20:25:56 +0100

    Forming documentation by sting comments

--- meow.py
+++ meow.py
@@ -1,6 +1,15 @@
-def meow(n: int):
-    for _ in range(n):
-        print("meow")
+def meow(n: int) -> str:
+    """ 
+    Moew n times. 
+
+    :param n: Number of times to meow
+    :type n: int
+    :raise TypeError: If n is not an int
+    :return: A string of meows, one per line
+    :rtype: str
+    """
+    return "meow\n" * 3
 
 number: int = input("Number: ")
-meow(number)
+meows: str = meow(number)
+print(meows)
