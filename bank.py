4eaef5a048667d04067360c0951816c05217d653
liutkevych <oli@keepit.com>
2023-11-28 19:21:04 +0100

    Use class instead of global variable

--- bank.py
+++ bank.py
@@ -1,18 +1,24 @@
-balance = 0
+class Account:
+    def __init__(self):
+        self._balance = 0
+    
+    @property
+    def balance(self):
+        return self._balance
+
+    def deposit(self, n):
+        self._balance += n
+
+    def withdraw(self, n):
+        self._balance -= n
 
 def main():
-    print("balance:", balance)
-    deposit(100)
-    withdrow(50)
-    print("balance:", balance)
+    account = Account()
+    print("Balance:", account.balance)
+    account.deposit(100)
+    account.withdraw(50)
+    print("Balance:", account.balance)
 
-def deposit(n):
-    global balance
-    balance += n
-
-def withdrow(n):
-    global balance
-    balance -= n
 
 if __name__ == "__main__":
     main()
