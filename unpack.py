0f428725e342ad06c6d3f99fbddc86f6155fd999
liutkevych <oli@keepit.com>
2023-11-29 11:24:10 +0100

    Variatic arguments in functions at unpack.py

--- unpack.py
+++ unpack.py
@@ -1,7 +1,6 @@
-def total(galleons, sickles, knuts):
-    return (galleons * 17 + sickles) * 29 + knuts
+def f(*args, **kwargs):
+    print("Positional:", args)
+    print("Named:", kwargs)
 
 
-coins = {"galleons": 100, "sickles": 50, "knuts": 25}
-
-print(total(**coins), "Knuts")
+f(100, 50, 25, 5, name="Alex")
