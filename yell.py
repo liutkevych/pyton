550a725d520a36ae4684d0e8cbb4ecb6c6bb4807
liutkevych <oli@keepit.com>
2023-11-30 13:21:09 +0100

    Add map functionality to yell.py

--- yell.py
+++ yell.py
@@ -1,11 +1,9 @@
 def main():
-    yell(["This", "is", "CS50"])
+    yell("This", "is", "CS50")
 
 
-def yell(words):
-    uppercased = []
-    for word in words:
-        uppercased.append(word.upper())
+def yell(*words):
+    uppercased = map(str.upper, words)
     print(*uppercased)
 
 
